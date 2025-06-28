e94170bca12273dd63796f1d1d3d0e0fe2842f87
solomon-webb <towolawisolomon09@gmail.com>
2025-06-28 20:55:00 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("solo-cache-v1").then(cache =>
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/sys.css",
+        "/manifest.json"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
